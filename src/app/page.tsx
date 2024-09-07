5c747b76b1277daa02b9062fb7fbcdbff48c9700
子麟 陳 <karta1027710@gmail.com>
2024-09-08 04:13:07 +0800

    [fix] Fix index redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { useRouter } from "next/navigation";
 export default function Home() {
   const router = useRouter();
 
-  router.push("/login");
+  router.push("/main-page");
 
   return <></>;
 }
