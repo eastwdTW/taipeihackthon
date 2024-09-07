347f51c6ac8fb724fea9695656445f2dddb4279b
Derrick Huang <derrickyi02@gmail.com>
2024-09-07 12:07:07 +0800

    WIP

--- src/app/regist/page.tsx
+++ src/app/regist/page.tsx
@@ -1,21 +1,13 @@
 'use client';
 import {Button, Card, Col, Flex, Form, Input, Row} from 'antd';
-import {useForm} from 'antd/lib/form/Form';
 import {EyeInvisibleOutlined, EyeTwoTone, UserOutlined} from '@ant-design/icons';
 import React, {useState} from 'react';
-import {useRouter} from 'next/navigation';
 
 export default function Login() {
-    const router = useRouter();
-    const [form] = useForm();
+    const [form] = Form.useForm();
     const [passwordVisible, setPasswordVisible] = useState(false);
-
-    const handleRegister = () => {
-        router.push('/regist');
-    }
-
     return (
-        <Flex justify={'center'} align={"center"} style={{height: '100vh', backgroundColor: '#f0f0f0'}}>
+        <Flex justify={'center'} align={"center"} style={{height: '100vh', backgroundColor: '#f0f0f0f0'}}>
             <Card
                 styles={{
                     header: {
@@ -25,16 +17,32 @@ export default function Login() {
                     }
                 }}
                 style={{width: '80%', border: '2px solid #000', borderRadius: '10px'}}
-                title={<span>台北市復康巴士</span>}>
+                title={<span>台北市復康巴士 - 註冊</span>}>
                 <Row>
                     <Col span={24}>
                         <Form form={form}>
                             <Form.Item name="account">
-                                <Input prefix={<UserOutlined/>} placeholder={"請輸入帳號"}/>
+                                <Input prefix={<UserOutlined/>} placeholder={"請輸入註冊帳號"}/>
                             </Form.Item>
                             <Form.Item name="password">
                                 <Input.Password
-                                    placeholder="請輸入密碼"
+                                    placeholder="請輸入註冊密碼"
+                                    iconRender={(visible) => (visible ? <EyeTwoTone/> : <EyeInvisibleOutlined/>)}
+                                />
+                            </Form.Item>
+                            <Form.Item
+                                name="checkPassword"
+                                rules={[{
+                                    validator: (rule, value) => {
+                                        if (value !== form.getFieldValue('password')) {
+                                            return Promise.reject('密碼不一致');
+                                        } else {
+                                            return Promise.resolve();
+                                        }
+                                    }
+                                }]}>
+                                <Input.Password
+                                    placeholder="再次輸入註冊密碼"
                                     iconRender={(visible) => (visible ? <EyeTwoTone/> : <EyeInvisibleOutlined/>)}
                                 />
                             </Form.Item>
@@ -46,7 +54,7 @@ export default function Login() {
                                 <Button type={"link"} style={{padding: 0}}>忘記密碼</Button>
                             </div>
                             <div>
-                                <Button onClick={handleRegister} style={{marginRight: '5px'}}>註冊</Button>
+                                <Button style={{marginRight: '5px'}}>註冊</Button>
                                 <Button type="primary">登入</Button>
                             </div>
                         </Flex>
