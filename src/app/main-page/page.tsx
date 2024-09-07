d52f56dc444117aea02fce681e9f285ce340e471
子麟 陳 <karta1027710@gmail.com>
2024-09-07 15:19:04 +0800

    [feat] Modify Header and Footer

--- src/app/main-page/page.tsx
+++ src/app/main-page/page.tsx
@@ -1,12 +1,9 @@
 "use client";
-import { UnorderedListOutlined } from "@ant-design/icons";
 import { Layout, Image, Button, Row, Col, Flex } from "antd";
 import { useState } from "react";
 import dayjs from "dayjs";
 import { tryGetToken } from "../../../api/api";
 import { useRouter } from "next/navigation";
-
-const { Footer } = Layout;
 
 interface Announcement {
   id: number;
@@ -86,57 +83,6 @@ export default function MainPage() {
 
   return (
     <Layout>
-      <Row
-        style={{
-          padding: "10px 10px",
-          backgroundColor: "#fff",
-          width: "100%",
-          color: "#000",
-          boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)",
-          zIndex: 999,
-        }}
-      >
-        <Col span={4}>
-          <Flex
-            justify="center"
-            align="center"
-            style={{ width: "100%", height: "100%" }}
-          >
-            <UnorderedListOutlined
-              style={{ color: "#5bb3c4", fontSize: "1.5rem" }}
-            />
-          </Flex>
-        </Col>
-        <Col span={16}>
-          <Flex
-            justify="center"
-            align="center"
-            style={{ width: "100%", height: "100%" }}
-          >
-            <span style={{ fontWeight: "bolder", fontSize: "1.5rem" }}>
-              台北市復康巴士
-            </span>
-          </Flex>
-        </Col>
-        <Col span={4}>
-          <Flex
-            justify="center"
-            align="center"
-            style={{ width: "100%", height: "100%" }}
-          >
-            <Button
-              style={{
-                backgroundColor: "#5bb3c4",
-                border: 0,
-                color: "#fff",
-                fontWeight: "bolder",
-              }}
-            >
-              登入
-            </Button>
-          </Flex>
-        </Col>
-      </Row>
       <div
         style={{
           backgroundColor: "#fff",
@@ -238,26 +184,6 @@ export default function MainPage() {
           </Button>
         </Flex>
       </div>
-      <Footer
-        style={{
-          boxShadow: "0 -4px 8px rgba(0, 0, 0, 0.2)",
-          textAlign: "center",
-          width: "100%",
-          position: "absolute",
-          bottom: 0,
-          backgroundColor: "#fff",
-        }}
-      >
-        <p style={{ margin: 0, fontSize: "0.8rem", fontWeight: "bolder" }}>
-          presented by 聽說名字要七個字
-        </p>
-        <Image
-          src="/logo.svg"
-          width="100px"
-          preview={false}
-          style={{ marginTop: "10px" }}
-        />
-      </Footer>
     </Layout>
   );
 }
